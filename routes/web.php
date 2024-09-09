c7f70aeb29dcdecad0ea76c3cf6fab43af8dce9f
Francesco Dente <francesco.dente2003@gmail.com>
2024-09-09 16:52:07 +0200

    1.0.1: build: static data upload

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
 Route::get('/', function () {
-    return view('welcome');
+
+
+    return view('home');
+
+    // , compact('')
 });
